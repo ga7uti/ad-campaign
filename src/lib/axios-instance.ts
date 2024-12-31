5e824d3037ad7b045647db19eedd8d8911278359
Gautam <gautam@nirmata.tech>
2024-12-31 13:27:14 +0530

    added logout on token expired

--- src/lib/axios-instance.ts
+++ src/lib/axios-instance.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { paths } from '@/paths'; // Adjust the import path as necessary
+import { useRouter } from 'next/router';
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
@@ -12,15 +14,30 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('accessToken');
-    if (token) {
-      const authEndpoints = ['/token', '/reset-password', '/register'];
-      if (token && !authEndpoints.some((endpoint) => config.url?.includes(endpoint))) {
-        config.headers.Authorization = `Bearer ${token}`;
-      }
+    const authEndpoints = ['/token', '/reset-password', '/register'];
+    if (token && !authEndpoints.some((endpoint) => config.url?.includes(endpoint))) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
   },
-  (error:Error) => {
+  (error: Error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      // Token is expired or invalid, log out the user
+      localStorage.removeItem('accessToken');
+      localStorage.removeItem('refreshToken');
+      localStorage.removeItem('userType');
+      const router = useRouter();
+      router.replace(paths.auth.signIn);
+    }
     return Promise.reject(error);
   }
 );
