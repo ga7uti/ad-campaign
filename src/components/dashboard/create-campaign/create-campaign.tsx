40097ea153f07e64209cc26dc5737519e5f3d9d5
Gautam <gautam@nirmata.tech>
2025-01-07 19:39:00 +0530

    updated form for create campaign

--- src/components/dashboard/create-campaign/create-campaign.tsx
+++ src/components/dashboard/create-campaign/create-campaign.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable -- Disabling all Eslint rules for the file*/
 "use client";
 import { campaignClient } from '@/lib/campaign-client';
-import { Box, Button, Card, CardContent, FormControl, Grid, TextField, Radio, RadioGroup, FormControlLabel, Typography } from '@mui/material';
+import { Box, Button, Card, CardContent, FormControl, Grid, TextField, Radio, RadioGroup, FormControlLabel, Typography, Divider } from '@mui/material';
 import { styled } from '@mui/material/styles';
-import { Upload } from '@phosphor-icons/react';
+import { CaretUp, Upload } from '@phosphor-icons/react';
+import { CaretDown } from '@phosphor-icons/react/dist/ssr';
 import * as React from 'react';
 
 const VisuallyHiddenInput = styled('input')({
@@ -36,6 +38,9 @@ export default function CreateCampaign(): React.JSX.Element {
   const [images, setImages] = React.useState<number[]>([]);
   const [logos, setLogos] = React.useState<number[]>([]);
   const [isPending, setIsPending] = React.useState<boolean>(false);
+  const [showTargetingType, setShowTargetingType] = React.useState<boolean>(true);
+  const [showUploadSection, setShowUploadSection] = React.useState<boolean>(false);
+  const [showLocationDeviceSection, setShowLocationDeviceSection] = React.useState<boolean>(false);
   const [isImageClicked, setImageClicked] = React.useState<boolean>(false);
   const [isLogoClicked, setLogoClicked] = React.useState<boolean>(false);
   const [isLogoUploadSuccess, setLogoUpload] = React.useState<boolean>(false);
@@ -71,7 +76,7 @@ export default function CreateCampaign(): React.JSX.Element {
 
   return (
     <Box component="form" sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-      {/* Main Form Fields */}
+      {/* Main Form Fields
       <Grid container spacing={2}>
         {Object.keys(formValues).map((field) =>
           field !== "target_optimize" ? (
@@ -112,68 +117,211 @@ export default function CreateCampaign(): React.JSX.Element {
             </Grid>
           )
         )}
-      </Grid>
+      </Grid> */}
 
-      {/* File Upload image and logo */}
-      <Box>
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={6}>
-            <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-              <Grid>
+      {/* Targeting Type Card */}
+      <Card>
+        <CardContent>
+        <FormControl fullWidth>
+              <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <Typography variant="h6">Targeting Type</Typography>
                 <Button
-                  component="label"
-                  role={undefined}
-                  variant="contained"
-                  tabIndex={-1}
-                  startIcon={<Upload />}
-                >
-                    {isPending && isLogoClicked? 'Uploading...' : 'Upload Logo'}
-                    <VisuallyHiddenInput
-                    type="file"
-                    onChange={(e) => uploadFile(e,setLogos,'logo')}
-                    onClick={()=> setLogoClicked(true)}
-                  multiple
-                  />
-                </Button>
-                {isLogoUploadSuccess && (
-                  <Typography variant="body2" color="green" mt={2}>
-                    Logo uploaded successfully!
-                  </Typography>
-                )}
-                {/* {errors.root ? <Alert color="error">{errors.root.message}</Alert> : null}   */}
-              </Grid>
-            </Box>
-              
-          </Grid> 
-          <Grid item xs={12} md={6}>
-            <Box sx={{ display: 'flex', justifyContent: 'flex-start' }}>
-              <Grid>
-                  <Button
-                    component="label"
-                    role={undefined}
-                    variant="contained"
-                    tabIndex={-1}
-                    startIcon={<Upload />}
-                  >
-                    {isPending && isImageClicked ? 'Uploading...' : 'Upload Image'}
-                    <VisuallyHiddenInput
-                      type="file"
-                      onChange={(e) => uploadFile(e, setImages,'image')}
-                      onClick={()=> setImageClicked(true)}
-                    multiple
-                    />
-                  </Button>
-                  {isImageUploadSuccess && (
-                    <Typography variant="body2" color="green" mt={2}>
-                      Logo uploaded successfully!
-                    </Typography>
-                  )}
-                  {/* {errors.root ? <Alert color="error">{errors.root.message}</Alert> : null}   */}
-                </Grid>
+                  variant="text"
+                  onClick={() => setShowTargetingType((prev) => !prev)}
+                  startIcon={showTargetingType?<CaretUp/>: <CaretDown/>}
+                />
               </Box>
-          </Grid>
-        </Grid>
-      </Box>
+              {showTargetingType && (
+                <Grid container spacing={2} mt={2}>
+                  <Grid item xs={12} md={6}>
+                    <TextField
+                      id="input-age"
+                      placeholder="Enter age range"
+                      variant="outlined"
+                      fullWidth
+                    />
+                  </Grid>
+                  <Grid item xs={12} md={6}>
+                    <TextField
+                      id="input-location"
+                      select
+                      SelectProps={{ native: true }}
+                      variant="outlined"
+                      fullWidth
+                    >
+                      <option value="">Select Location</option>
+                      <option value="location1">Location 1</option>
+                      <option value="location2">Location 2</option>
+                    </TextField>
+                  </Grid>
+                  <Grid item xs={12} md={6}>
+                    <TextField
+                      id="input-device"
+                      select
+                      SelectProps={{ native: true }}
+                      variant="outlined"
+                      fullWidth
+                    >
+                      <option value="">Select Device</option>
+                      <option value="device1">Device 1</option>
+                      <option value="device2">Device 2</option>
+                    </TextField>
+                  </Grid>
+                  <Grid item xs={12} md={6}>
+                    <TextField
+                      id="input-environment"
+                      select
+                      SelectProps={{ native: true }}
+                      variant="outlined"
+                      fullWidth
+                    >
+                      <option value="">Select Environment</option>
+                      <option value="environment1">Environment 1</option>
+                      <option value="environment2">Environment 2</option>
+                    </TextField>
+                  </Grid>
+                </Grid>
+              )}
+          </FormControl>
+        </CardContent>
+      </Card>
+
+      {/* Targeting Type Card */}
+      <Card>
+        <CardContent>
+        <FormControl fullWidth>
+              <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <Typography variant="h6">Location & Device</Typography>
+                <Button
+                  variant="text"
+                  onClick={() => setShowLocationDeviceSection((prev) => !prev)}
+                  startIcon={showLocationDeviceSection?<CaretUp/>: <CaretDown/>}
+                />
+              </Box>
+              {showLocationDeviceSection && (
+                <Grid container spacing={2} mt={2}>
+                  <Grid item xs={12} md={6}>
+                    <TextField
+                      id="input-location"
+                      select
+                      SelectProps={{ native: true }}
+                      variant="outlined"
+                      fullWidth
+                    >
+                      <option value="">Select Language</option>
+                      <option value="location1">Language 1</option>
+                      <option value="location2">Language 2</option>
+                    </TextField>
+                  </Grid>
+                  <Grid item xs={12} md={6}>
+                    <TextField
+                      id="input-device"
+                      select
+                      SelectProps={{ native: true }}
+                      variant="outlined"
+                      fullWidth
+                    >
+                      <option value="">Select Carrier</option>
+                      <option value="device1">Jio</option>
+                      <option value="device2">Airtel</option>
+                    </TextField>
+                  </Grid>
+                  <Grid item xs={12} md={6}>
+                    <TextField
+                      id="input-environment"
+                      select
+                      SelectProps={{ native: true }}
+                      variant="outlined"
+                      fullWidth
+                    >
+                      <option value="">Select Device Price</option>
+                      <option value="environment1">Less thank 10K</option>
+                      <option value="environment2">10-15K</option>
+                    </TextField>
+                  </Grid>
+                </Grid>
+              )}
+          </FormControl>
+        </CardContent>
+      </Card>
+
+      {/* Upload Documents */}
+      <Card>
+        <CardContent>
+        <FormControl fullWidth>
+              <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <Typography variant="h6">Upload Documents</Typography>
+                <Button
+                  variant="text"
+                  onClick={() => setShowUploadSection((prev) => !prev)}
+                  startIcon={showUploadSection?<CaretUp/>: <CaretDown/>}
+                />
+              </Box>
+              {showUploadSection && (
+                <Grid container spacing={2}>
+                  <Grid item xs={12} md={6}>
+                    <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+                      <Grid>
+                        <Button
+                          component="label"
+                          role={undefined}
+                          variant="contained"
+                          tabIndex={-1}
+                          startIcon={<Upload />}
+                        >
+                            {isPending && isLogoClicked? 'Uploading...' : 'Upload Logo'}
+                            <VisuallyHiddenInput
+                            type="file"
+                            onChange={(e) => uploadFile(e,setLogos,'logo')}
+                            onClick={()=> setLogoClicked(true)}
+                          multiple
+                          />
+                        </Button>
+                        {isLogoUploadSuccess && (
+                          <Typography variant="body2" color="green" mt={2}>
+                            Logo uploaded successfully!
+                          </Typography>
+                        )}
+                        {/* {errors.root ? <Alert color="error">{errors.root.message}</Alert> : null}   */}
+                      </Grid>
+                    </Box>
+                      
+                  </Grid> 
+                  <Grid item xs={12} md={6}>
+                    <Box sx={{ display: 'flex', justifyContent: 'flex-start' }}>
+                      <Grid>
+                          <Button
+                            component="label"
+                            role={undefined}
+                            variant="contained"
+                            tabIndex={-1}
+                            startIcon={<Upload />}
+                          >
+                            {isPending && isImageClicked ? 'Uploading...' : 'Upload Image'}
+                            <VisuallyHiddenInput
+                              type="file"
+                              onChange={(e) => uploadFile(e, setImages,'image')}
+                              onClick={()=> setImageClicked(true)}
+                            multiple
+                            />
+                          </Button>
+                          {isImageUploadSuccess && (
+                            <Typography variant="body2" color="green" mt={2}>
+                              Logo uploaded successfully!
+                            </Typography>
+                          )}
+                          {/* {errors.root ? <Alert color="error">{errors.root.message}</Alert> : null}   */}
+                        </Grid>
+                      </Box>
+                  </Grid>
+                </Grid>
+              )}
+          </FormControl>
+        </CardContent>
+      </Card>
+
+
+      
       <Box sx={{ textAlign: "center", mt: 3 }}>
         <Button variant="contained" color="primary" onClick={handleSubmit}>
           Create Campaign
