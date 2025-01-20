7f9f1d545b5d8c3444b704d08385b7d199bd1838
Gautam <gautam@nirmata.tech>
2025-01-20 23:55:33 +0530

    added progress bar

--- src/components/dashboard/account/account-details-form.tsx
+++ src/components/dashboard/account/account-details-form.tsx
@@ -4,7 +4,7 @@
 import { accountClient } from '@/lib/account-client';
 import { User } from '@/types/user';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Alert } from '@mui/material';
+import { Alert, Box, CircularProgress } from '@mui/material';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -33,6 +33,7 @@ export function AccountDetailsForm(): React.JSX.Element {
 
   const [user, setUser] = React.useState<User | null>(null);
   const [isPending, setIsPending] = React.useState<boolean>(false);
+  const [loading, setIsLoading] = React.useState<boolean>(false);
   const [isProfileUpdated,setIsProfileUpdated] = React.useState<boolean>(false);
   
   const {
@@ -44,13 +45,16 @@ export function AccountDetailsForm(): React.JSX.Element {
   } = useForm<Values>({ defaultValues: user || {}, resolver: zodResolver(schema) });
 
   async function fetchUser() {
+    setIsLoading(true);
     try {
       const response = await accountClient.getUser();
       setUser(response);
       reset(response);  
     } catch (error:any) {
       setError('root', { type: 'server', message: error.message});
-     }
+    }finally{
+      setIsLoading(false);
+    }
   }  
   
   const onSubmit = React.useCallback(
@@ -80,58 +84,71 @@ export function AccountDetailsForm(): React.JSX.Element {
       <Card>
         <CardHeader subheader="The information can be edited" title="Profile" />
         <Divider />
-        <CardContent>
-          <Grid container spacing={3}>
-            <Grid md={6} xs={12}>
-              <Controller
-                control={control}
-                name="first_name"
-                render={({ field }) => (
-                  <FormControl error={Boolean(errors.first_name)}>
-                    <OutlinedInput {...field} defaultValue={user?.first_name} type="text" />
-                    {errors.first_name ? <FormHelperText>{errors.first_name.message}</FormHelperText> : null}
-                  </FormControl>
-                )}
-              />
-            </Grid>
-            <Grid md={6} xs={12}>
-              <Controller
+        {loading ? 
+          <Box  
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: '1rem',
+              marginBottom:'1rem'
+            }}>
+            <CircularProgress />
+          </Box>
+        :
+          <CardContent>
+            <Grid container spacing={3}>
+              <Grid md={6} xs={12}>
+                <Controller
                   control={control}
-                  name="last_name"
+                  name="first_name"
                   render={({ field }) => (
-                    <FormControl error={Boolean(errors.last_name)}>
-                      <OutlinedInput {...field} defaultValue={user?.last_name} type="text" />
-                      {errors.last_name ? <FormHelperText>{errors.last_name.message}</FormHelperText> : null}
+                    <FormControl error={Boolean(errors.first_name)}>
+                      <OutlinedInput {...field} defaultValue={user?.first_name} type="text" />
+                      {errors.first_name ? <FormHelperText>{errors.first_name.message}</FormHelperText> : null}
                     </FormControl>
                   )}
                 />
+              </Grid>
+              <Grid md={6} xs={12}>
+                <Controller
+                    control={control}
+                    name="last_name"
+                    render={({ field }) => (
+                      <FormControl error={Boolean(errors.last_name)}>
+                        <OutlinedInput {...field} defaultValue={user?.last_name} type="text" />
+                        {errors.last_name ? <FormHelperText>{errors.last_name.message}</FormHelperText> : null}
+                      </FormControl>
+                    )}
+                  />
+              </Grid>
+              <Grid md={6} xs={12}>
+                <Controller
+                  control={control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormControl error={Boolean(errors.email)}>
+                      <OutlinedInput disabled {...field} defaultValue={user?.email} type="email" />
+                      {errors.email ? <FormHelperText>{errors.email.message}</FormHelperText> : null}
+                    </FormControl>
+                  )}
+                />
+              </Grid>
+              <Grid md={6} xs={12}>
+                <Controller
+                  control={control}
+                  name="phone_no"
+                  render={({ field }) => (
+                    <FormControl error={Boolean(errors.phone_no)}>
+                      <OutlinedInput {...field} defaultValue={user?.phone_no} type="number" />
+                      {errors.phone_no ? <FormHelperText>{errors.phone_no.message}</FormHelperText> : null}
+                    </FormControl>
+                  )}
+                />
+              </Grid>
             </Grid>
-            <Grid md={6} xs={12}>
-              <Controller
-                control={control}
-                name="email"
-                render={({ field }) => (
-                  <FormControl error={Boolean(errors.email)}>
-                    <OutlinedInput disabled {...field} defaultValue={user?.email} type="email" />
-                    {errors.email ? <FormHelperText>{errors.email.message}</FormHelperText> : null}
-                  </FormControl>
-                )}
-              />
-            </Grid>
-            <Grid md={6} xs={12}>
-              <Controller
-                control={control}
-                name="phone_no"
-                render={({ field }) => (
-                  <FormControl error={Boolean(errors.phone_no)}>
-                    <OutlinedInput {...field} defaultValue={user?.phone_no} type="number" />
-                    {errors.phone_no ? <FormHelperText>{errors.phone_no.message}</FormHelperText> : null}
-                  </FormControl>
-                )}
-              />
-            </Grid>
-          </Grid>
-        </CardContent>
+          </CardContent>
+          }
         <Divider />
         <CardActions sx={{ justifyContent: 'flex-end' }}>
         {errors.root ? <Alert color="error">{errors.root.message}</Alert> : null}  
